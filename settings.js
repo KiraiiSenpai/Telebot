9f56417650bd33a86faa413f59d17ef34fd77cd6
KiraiiSenpai <111110504+KiraiiSenpai@users.noreply.github.com>
2024-05-02 05:39:27 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,11 +17,11 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
+global.BOT_TOKEN = "6829631375:AAEoXgii1XcSMaM4Jzkv3CVfFYlc9zgybZ0" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "𝐁𝐥𝐚𝐜𝐤𝐕𝐨𝐢𝐝𝐁𝐨𝐭" //your bot name
 global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
-global.OWNER_NUMBER = "62887435047326" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.OWNER_NUMBER = "5521985050668" //your telegram number
+global.OWNER = ["https://t.me/sarcasm1998", "https://t.me/f3x666"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
